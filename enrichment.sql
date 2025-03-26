3827b002287fa853cdb3e2d9a794824cb5f09d36
Bolaji Wahab <bolaji.wahab@n26.com>
2025-03-26 23:38:07 +0100

    more bootstrap

--- enrichment.sql
+++ enrichment.sql
@@ -1,16 +1,12 @@
 CREATE TABLE partitions.enrichment
     PARTITION OF public.enrichment
     FOR VALUES FROM ('2023-01-01 00:00:00+00') TO ('2024-01-01 00:00:00+00');
-
 CREATE TABLE partitions.enrichment
     PARTITION OF public.enrichment
     FOR VALUES FROM ('2024-01-01 00:00:00+00') TO ('2025-01-01 00:00:00+00');
-
 CREATE TABLE partitions.enrichment
     PARTITION OF public.enrichment
     FOR VALUES FROM ('2025-01-01 00:00:00+00') TO ('2026-01-01 00:00:00+00');
-
 CREATE TABLE partitions.enrichment
     PARTITION OF public.enrichment
     FOR VALUES FROM ('2026-01-01 00:00:00+00') TO ('2027-01-01 00:00:00+00');
-
