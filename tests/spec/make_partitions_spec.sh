6d6fb6e59e4c921d545576c6107097c47bddfa14
Bolaji Wahab <bolajiwahab23@gmail.com>
2025-04-19 06:13:33 +0200

    chore: fix pre-commit

--- tests/spec/make_partitions_spec.sh
+++ tests/spec/make_partitions_spec.sh
@@ -1,3 +1,5 @@
+# shellcheck shell=sh
+
 Describe "pgp-make-partitions"
   # NOTE: Generating test cases dynamically from files
   Parameters:dynamic
@@ -10,7 +12,7 @@ Describe "pgp-make-partitions"
   Data < "$1" # NOTE: Treat a file as stdin data
 
   It "makes partitions with valid config ($1)"
-    When run script pgp-make-partitions -c $1
+    When run script pgp-make-partitions -c "$1"
     The output should be present # See: https://github.com/shellspec/shellspec/issues/122
     diff -u "$2" "$3"
     The status should be success
