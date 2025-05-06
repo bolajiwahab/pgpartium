66d63d081437dfc07c4c243b72e16119e542a668
Bolaji Wahab <bolajiwahab23@gmail.com>
2025-05-06 13:52:19 +0200

    chore: use flyway for migration

--- tests/setup.sh
+++ tests/setup.sh
@@ -42,28 +42,28 @@ fi
 # Append .0 if pgversion is a single-digit version e.g 9
 [[ "${pgversion}" =~ ^[0-9]$ ]] && pgversion+=.0
 
-echo "Creating pgtap extension"
-apt.postgresql.org.sh -i -v "${pgversion}"
+echo "INFO: Creating pgtap extension"
+# apt.postgresql.org.sh -i -v "${pgversion}"
 apt-get install -y postgresql-"${pgversion}"-pgtap
 psql -v "ON_ERROR_STOP=1" --quiet --username=postgres --dbname=postgres -c "CREATE EXTENSION pgtap;"
 
-echo "Setting up shellspec"
+echo "INFO: Setting up shellspec"
 wget --no-verbose https://github.com/shellspec/shellspec/archive/0.28.1.tar.gz --output-document=/tmp/shellspec-0.28.1.tar.gz
 tar xzf /tmp/shellspec-0.28.1.tar.gz -C /tmp
 ln -s /tmp/shellspec-0.28.1/bin/shellspec /usr/bin/shellspec
 
-echo "Creating tablespace pgpartium"
+echo "INFO: Creating tablespace pgpartium"
 mkdir -p /var/lib/postgresql/tablespaces/pgpartium
 chown -R postgres:postgres /var/lib/postgresql/tablespaces/pgpartium
 psql -v "ON_ERROR_STOP=1" --quiet --username=postgres --dbname=postgres -c "CREATE TABLESPACE pgpartium LOCATION '/var/lib/postgresql/tablespaces/pgpartium';"
 
-echo "Setting up schema"
-psql -v "ON_ERROR_STOP=1" --quiet --single-transaction --username=postgres --dbname=postgres -f tests/resources/mock.sql -f tests/resources/schema.sql
+# echo "Setting up schema"
+# psql -v "ON_ERROR_STOP=1" --quiet --single-transaction --username=postgres --dbname=postgres -f tests/resources/mock.sql -f tests/resources/schema.sql
 
-echo "Setting up infra"
+echo "INFO: Setting up infra"
 pgp-setup-infra
 
 # We need to include mock in our search_path
-echo "Adding mock to search_path"
+echo "INFO: Adding mock to search_path"
 psql -v "ON_ERROR_STOP=1" --quiet --username=postgres --dbname=postgres -c "ALTER SYSTEM SET search_path = mock, public, pg_catalog;"
 pg_ctlcluster "${pgversion}" pgpartium reload
