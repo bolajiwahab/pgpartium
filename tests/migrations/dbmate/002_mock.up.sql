5a0d12632622bbe76874905e4c280cdf103908e4
Bolaji Wahab <bolajiwahab23@gmail.com>
2025-05-07 02:14:48 +0200

    test: add migration files for dbmate

---
+++ tests/migrations/dbmate/002_mock.up.sql
@@ -0,0 +1,13 @@
+CREATE SCHEMA mock;
+
+-- We are mocking now() to return a fixed timestamptz value
+-- as the core function pgpartium.make_partitions uses now()
+-- to calculate the partitioning range.
+CREATE OR REPLACE FUNCTION mock.now ()
+RETURNS timestamptz
+LANGUAGE SQL
+IMMUTABLE
+PARALLEL SAFE
+AS $BODY$
+    SELECT CAST('2025-03-01 00:00:00' AS timestamptz);
+$BODY$;
