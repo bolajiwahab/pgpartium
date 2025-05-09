aca14b78f81efaba6dff70875aceeb96739b08cc
Bolaji Wahab <bolajiwahab23@gmail.com>
2025-05-10 00:51:05 +0200

    docs: generate schema doc

---
+++ docs/schema_doc_generator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+python3 -m venv .jsfh_env
+
+# shellcheck source=/dev/null
+source .jsfh_env/bin/activate
+
+pip3 install json-schema-for-humans==1.4.1
+
+generate-schema-doc schema.json --config-file docs/schema_doc.yaml docs/docs/schema.html
+
+deactivate
+rm -rf .jsfh_env
