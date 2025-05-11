c35a011e636d66974fc3ff050854f200dee5f32d
Bolaji Wahab <bolajiwahab23@gmail.com>
2025-05-11 21:46:10 +0200

    feat: replace concatenate with format

--- sql/15__make_partitions.sql
+++ sql/15__make_partitions.sql
@@ -461,11 +461,11 @@ $SQL$,          v_partition_schema                                              
                 END
             );
 
-            IF v_indexes IS NOT NULL THEN
-                v_ddl := format(E'%1$s\n%2$s', v_ddl, v_indexes);
+            -- IF v_indexes IS NOT NULL THEN
+            v_ddl := format(E'%1$s\n%2$s', v_ddl, v_indexes);
                 -- v_ddl := COALESCE(v_ddl || E' (\n' || v_constraints || E'\n    )', '')
                 -- v_ddl := v_ddl || E'\n' || v_indexes;
-            END IF;
+            -- END IF;
 
             IF v_triggers IS NOT NULL THEN
                 v_ddl := v_ddl || E'\n' || v_triggers;
